4baef77149ea666aa44d92702d72a8924a92c7d0
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-05-09 12:04:38 -0300

    Exercise completed

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public int cantclones;
+    
+    public GameObject objectToClone;
+
+    
+
+    public void CloneObject()
+    {
+        int contador = 0;
+        while (contador <= cantclones)
+        {
+            Instantiate(objectToClone);
+            contador++;
+        }
+    }
+}
